a31164f2f99b9df41f9085c842f7b8d1e3c4c953
ErickBios <erickbiosk12@gmail.com>
2023-08-08 21:28:13 -0300

    Correções

--- Confeitaria_NoMilk/Models/CATEGORIA.cs
+++ Confeitaria_NoMilk/Models/CATEGORIA.cs
@@ -1,9 +1,23 @@
-﻿namespace Confeitaria_NoMilk.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Confeitaria_NoMilk.Models
 {
+
+	[Table("CATEGORIA")]
 	public class CATEGORIA
 	{
+		[Key]
         public int CATEGORIA_ID { get; set; }
+
+		[StringLength(100, ErrorMessage = "O tamanho máximo é 100 caracteres")]
+		[Required(ErrorMessage = "Informe o nome da categoria")]
+		[Display(Name = "Nome")]
         public string CATEGORIA_NOME { get; set; }
+
+		[StringLength(200, ErrorMessage = "O tamanho máximo é 200 caracteres")]
+		[Required(ErrorMessage = "Informe a descrição da categoria")]
+		[Display(Name = "Descrição")]
         public string DESCRICAO { get; set; }
 
         // relacionamento de um para muitos lanches 
